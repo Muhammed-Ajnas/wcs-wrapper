1beee91190fff0c12d09c81f55947f3f4c4a751c
Muhammed Ajnas O K <ajnas@litmus7.com>
2025-09-19 10:07:43 +0530

    misc changes

--- src/schemas/validationSchema.ts
+++ src/schemas/validationSchema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
 export const getProductAttributeValidationSchema = z.object({
-  productId: z.coerce.bigint().positive(),
-  storeId: z.coerce.number().int().positive(),
+  productId: z.coerce.bigint("Product Id needs to be a long value").positive("Product Id cannot be negative"),
+  storeId: z.coerce.number("Store Id Id needs to be an integer value").int().positive("Store Id cannot be negative"),
 });
--- src/util/mapper.ts
+++ src/util/mapper.ts
@@ -2,10 +2,6 @@ import { ProductAttributesPayload, WcsAttributeList } from "#schemas/wcsSchemas.
 
 export const mapWcsToOrchestrator = (wcs: WcsAttributeList): ProductAttributesPayload => {
   return {
-    attributes: (wcs.data ?? []).map((a) => ({
-      name: a.attributeName,
-      usage: a.usage,
-      value: a.value,
-    })),
+    attributes: (wcs.data ?? []).map((a) => ({ name: a.attributeName, usage: a.usage, value: a.value })),
   };
 };
